22aec3ecaa0aec628036f88bb5b2ce24291dd702
farid45219 <refrigerator.rni.eed7@gmail.com>
2024-01-15 14:21:18 +0600

    Error handled in setting time

--- rtc.c
+++ rtc.c
@@ -22,36 +22,43 @@ void RTC_Set_12H_PM(void){
 }
 
 void RTC_Set_Time(uint8_t hr, uint8_t min, uint8_t sec){
-  uint8_t tmp0=0,tmp1=0,tmp2=0;
+  uint8_t tmp0=0,tmp1=0,tmp2=0, error=0;
+  uint8_t hr_reg=0, min_reg=0, sec_reg=0;
   tmp0=I2C_Read_Register(0x02);
   tmp0&=0x60;
   tmp1=hr/10;
   tmp2=hr%10;
-  if(tmp0 & (1<<6)){                    //12H Mode
+  if(tmp0 & (1<<6)){          //12H
     if(hr<=12){
-	  I2C_Write_Register(0x02,tmp0|(tmp1<<4)|tmp2);
+	  hr_reg=tmp0|(tmp1<<4)|tmp2;
 	}else{
-	  //handle error
+	  error=0x01;
 	}
-  }else{                               //24H 
+  }else{                      //24H 
     tmp0&=0x40;
-	I2C_Write_Register(0x02,(tmp1<<4)|tmp2);
+	hr_reg=(tmp1<<4)|tmp2;
   }
   
   tmp1=min/10;
   tmp2=min%10;
   if(min<=59){
-    I2C_Write_Register(0x01,(tmp1<<4)|tmp2);
+    min_reg=(tmp1<<4)|tmp2;
   }else{
-    //handle error
+    error=0x02;
   }
   
   tmp1=sec/10;
   tmp2=sec%10;
   if(sec<=59){
-    I2C_Write_Register(0x00,(tmp1<<4)|tmp2);
+    sec_reg=(tmp1<<4)|tmp2;
   }else{
-    //handle error
+    error=0x03;
+  }
+  
+  if(error==0){
+    I2C_Write_Register(0x02,hr_reg);
+	I2C_Write_Register(0x01,min_reg);
+	I2C_Write_Register(0x00,sec_reg);
   }
 }
 
@@ -68,7 +75,7 @@ void RTC_Set_Time_With_Format(uint8_t hr, uint8_t min, uint8_t sec, uint8_t hr_c
   RTC_Set_Time( hr, min, sec);
 }
   
-uint8_t RTC_Get_AmPm(void){        //0:AM, 1:PM
+uint8_t RTC_Get_AmPm(void){  //0:AM, 1:PM
   uint8_t tmp0=I2C_Read_Register(0x02);
   if(tmp0 & (1<<5)){
     return 1;
@@ -80,11 +87,11 @@ uint8_t RTC_Get_AmPm(void){        //0:AM, 1:PM
 uint8_t RTC_Get_Hr(void){
   uint8_t tmp0=0, hr=0;
   tmp0=I2C_Read_Register(0x02);
-  if(tmp0 & (1<<6)){               //12H
+  if(tmp0 & (1<<6)){           //12H
     hr = ((tmp0 & (1<<4))>>4);
 	hr*= 10;
 	hr+= (tmp0 & 0x0F);
-  }else{                           //24H
+  }else{                       //24H
     hr = (tmp0 & 0x30)>>4;
     hr*= 10;
 	hr+= (tmp0 & 0x0F);
